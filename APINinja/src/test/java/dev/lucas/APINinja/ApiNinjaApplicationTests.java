437bc0a8486b70df9058ddadba10c222668e34c0
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-09-05 22:35:04 -0300

    inicio do projeto

---
+++ APINinja/src/test/java/dev/lucas/APINinja/ApiNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lucas.APINinja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
